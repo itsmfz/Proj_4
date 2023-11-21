8fe7a3fc6f06dbf7138a75a9a27eec377f71c258
itsmfz <fawwaamd@mail.uc.edu>
2023-11-21 01:00:15 -0500

    trial

--- src/test1.py
+++ src/test1.py
@@ -1,23 +1,59 @@
-def compute_target_binary(difficulty):
-    """
-    Compute the target value as a binary string based on the given difficulty.
+import os
+import secrets
+from hashlib import sha256
+from base64 import b64encode, b64decode
+import glob
+import hashlib
+import time
+
+def targetGen(dif):
+    if not 0 <= dif <= 256:
+        raise ValueError("Input value 'dif' must be between 0 and 256 (inclusive).")
+
+    # Create the binary string with dif number of 0's followed by (256 - dif) number of 1's
+    binary_string = f"{'0' * dif}{'1' * (256 - dif)}"
+
+    # Write the binary string to a text file
+    with open('data/target.txt', 'w') as file:
+        file.write(binary_string)
+
+    return binary_string
+
+def solutionGen(m, t):
     
-    Parameters:
-    - difficulty: Number of leading zeros required in the hash
+    start_time = time.time()
     
-    Returns:
-    - target_binary: Binary string representing the target value for the Proof of Work
-    """
-    if not 0 <= difficulty <= 256:
-        raise ValueError("Difficulty must be between 0 and 256 (inclusive).")
+    s = 0
+    while True:
+        message = f"{m}{s}".encode('utf-8')
+        hash_result = hashlib.sha256(message).hexdigest()
 
-    # Compute the target binary string
-    target_binary = '0' * difficulty + '1' * (256 - difficulty)
-    return target_binary
+        print(f"Trying s = {s}, Hash: {hash_result}")
 
-# Example usage:
-difficulty_level = 4  # Adjust difficulty level as needed
-target_binary_value = compute_target_binary(difficulty_level)
+        if int(hash_result, 16) <= int(t, 2):
+            end_time = time.time()
+            total_time = end_time - start_time
+            return s, total_time
 
-print(f"Difficulty: {difficulty_level}")
-print(f"Target Binary Value: {target_binary_value}")
+        s += 1
+
+def main():
+    d = int(input("Enter the value of D "))
+    print("\nTarget:")
+    print(targetGen(d))
+
+    file_path = "data/input.txt"
+    with open(file_path, 'r') as file:
+        message = file.read()
+
+    file_path = "data/target.txt"
+    with open(file_path, 'r') as file:
+        target_value = file.read()
+
+    print("Target value in binary:", target_value)
+    found_s, total_time = solutionGen(message, target_value)
+    print(f"Found s: {found_s}")
+    print(f"Total time: {total_time:.4f} seconds")
+
+if __name__ == "__main__":
+    main()
--- src/test2.py
+++ src/test2.py
@@ -1,5 +1,9 @@
-import hashlib
 import os
+import secrets
+from hashlib import sha256
+from base64 import b64encode, b64decode
+import glob
+import hashlib
 import time
 
 def targetGen(dif):
@@ -9,66 +13,48 @@ def targetGen(dif):
     # Create the binary string with dif number of 0's followed by (256 - dif) number of 1's
     binary_string = f"{'0' * dif}{'1' * (256 - dif)}"
 
-    # Write the binary value to a file
+    # Write the binary string to a text file
     with open('data/target.txt', 'w') as file:
         file.write(binary_string)
 
     return binary_string
 
 def solutionGen(m, t):
+    
     start_time = time.time()
-
-    s = 0
+    
+    s = "0"
     while True:
         message = f"{m}{s}".encode('utf-8')
         hash_result = hashlib.sha256(message).hexdigest()
 
         print(f"Trying s = {s}, Hash: {hash_result}")
 
-        if int(hash_result, 16) <= t:
+        if int(hash_result, 16) <= int(t, 2):
             end_time = time.time()
             total_time = end_time - start_time
-
-            # Write the solution to a file
-            with open('data/solution.txt', 'w') as file:
-                file.write(str(s))
-
             return s, total_time
 
-        s += 1
-
-def verification(m, s, t):
-    message = f"{m}{s}".encode('utf-8')
-    hash_result = hashlib.sha256(message).hexdigest()
-
-    if int(hash_result, 16) <= t:
-        return 1
-    else:
-        return 0
+        # Increment s as a binary string
+        s = bin(int(s, 2) + 1)[2:]
 
 def main():
-    d = int(input("Enter the value of D: "))
+    d = int(input("Enter the value of D "))
     print("\nTarget:")
     print(targetGen(d))
 
-    # Read message from file
-    with open('data/input.txt', 'r') as file:
+    file_path = "data/input.txt"
+    with open(file_path, 'r') as file:
         message = file.read()
 
-    # Read target from file
-    with open('data/target.txt', 'r') as file:
+    file_path = "data/target.txt"
+    with open(file_path, 'r') as file:
         target_value = file.read()
 
-    target = int(target_value, 2)  # Convert binary string to integer
-    print(target)
-
-    found_s, total_time = solutionGen(message, target)
-    print(f"\nFound s: {found_s}")
+    print("Target value in binary:", target_value)
+    found_s, total_time = solutionGen(message, target_value)
+    print(f"Found s: {found_s}")
     print(f"Total time: {total_time:.4f} seconds")
-
-    # Verification
-    result = verification(message, found_s, target)
-    print(f"\nVerification Result: {result}")
 
 if __name__ == "__main__":
     main()
