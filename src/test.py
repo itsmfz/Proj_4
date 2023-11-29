1ef9fbe25b92c830e1be64da8d9b92da6f4ba869
itsmfz <fawwaamd@mail.uc.edu>
2023-11-29 15:37:21 -0500

    Final

--- src/test.py
+++ src/test.py
@@ -1,8 +1,8 @@
-import os            # Operating system-related functions
-import secrets       # Cryptographically strong random number generation
-from hashlib import sha256  # Hash function for secure hashing
-from base64 import b64encode, b64decode  # Encoding/decoding data in base64 format
-import glob # Import the glob module for file path pattern matching
+import os
+import secrets
+from hashlib import sha256
+from base64 import b64encode, b64decode
+import glob
 import hashlib
 import time
 
@@ -13,17 +13,17 @@ def targetGen(dif):
     # Create the binary string with dif number of 0's followed by (256 - dif) number of 1's
     binary_string = f"{'0' * dif}{'1' * (256 - dif)}"
 
-    # Write the binary and decimal values to a text file
+    # Write the binary string to a text file
     with open('data/target.txt', 'w') as file:
         file.write(binary_string)
 
-
     return binary_string
 
-def solutionGen(m, t):
-    
+def solutionGen(m, target_value):
     start_time = time.time()
-    
+
+    # Convert the binary string to an integer
+    t = int(target_value, 2)
     s = 0
     while True:
         message = f"{m}{s}".encode('utf-8')
@@ -38,27 +38,47 @@ def solutionGen(m, t):
 
         s += 1
 
+def verifySolution(m, s, t):
+    message = f"{m}{s}".encode('utf-8')
+    hash_result = hashlib.sha256(message).hexdigest()
+
+    if int(hash_result, 16) <= t:
+        return 1  # Valid solution
+    else:
+        return 0  # Invalid solution
+
 def main():
+    # Read the difficulty value from user input
     d = int(input("Enter the value of D "))
+
+    # Generate and print the target
     print("\nTarget:")
-    print(targetGen(d))
+    target_value = targetGen(d)
+    print(target_value)
 
-
-    file_path = "data/input.txt"  # Replace with the actual file path
+    # Read the input message from file
+    file_path = "data/input.txt"
     with open(file_path, 'r') as file:
-        # Read the contents of the file into a variable
         message = file.read()
 
-    file_path = "data/target.txt"  # Replace with the actual file path
+    # Read the target from file
+    file_path = "data/target.txt"
     with open(file_path, 'r') as file:
-        # Read the contents of the file into a variable
         target_value = file.read()
 
-    target = int(target_value)
-    print(target)
-    found_s, total_time = solutionGen(message, target)
+    # Generate, print, and write the solution to file
+    found_s, total_time = solutionGen(message, target_value)
     print(f"Found s: {found_s}")
+
+    # Write the solution to file
+    with open("data/solution.txt", 'w') as file:
+        file.write(str(found_s))  # Convert found_s to string before writing
+
     print(f"Total time: {total_time:.4f} seconds")
+
+    # Verification
+    verification_result = verifySolution(message, found_s, int(target_value, 2))
+    print(f"Verification result: {verification_result}")
 
 if __name__ == "__main__":
     main()
